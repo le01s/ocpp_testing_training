2e757547b1fa16409091f02ff1253b186e9ee27d
le01s <nkbotcman@gmail.com>
2025-04-24 12:39:16 +0300

    [UPD] Общий рефач

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 import asyncio
-import logging
 from datetime import datetime, timezone
-
-try:
-    import websockets
-except ModuleNotFoundError:
-    print("This example relies on the 'websockets' package.")
-    print("Please install it by running: ")
-    print()
-    print(" $ pip install websockets")
-    import sys
-
-    sys.exit(1)
+import logging
 
 from ocpp.routing import on
 from ocpp.v16 import ChargePoint as cp
 from ocpp.v16 import call_result
 from ocpp.v16.enums import Action, RegistrationStatus
+import websockets
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,7 +14,10 @@ logging.basicConfig(level=logging.INFO)
 class ChargePoint(cp):
     @on(Action.boot_notification)
     def on_boot_notification(
-        self, charge_point_vendor: str, charge_point_model: str, **kwargs
+        self,
+        charge_point_vendor: str,
+        charge_point_model: str,
+        **kwargs
     ):
         return call_result.BootNotification(
             current_time=datetime.now(timezone.utc).isoformat(),
@@ -38,20 +31,29 @@ async def on_connect(websocket):
     instance and start listening for messages.
     """
     try:
-        requested_protocols = websocket.request.headers["Sec-WebSocket-Protocol"]
+        requested_protocols = websocket.request.headers[
+            "Sec-WebSocket-Protocol"
+        ]
     except KeyError:
-        logging.error("Client hasn't requested any Subprotocol. "
-                      "Closing Connection")
+        logging.error(
+            "Client hasn't requested any Subprotocol. "
+            "Closing Connection"
+        )
         return await websocket.close()
     if websocket.subprotocol:
-        logging.info(f"Protocols Matched: {websocket.subprotocol}")
+        logging.info(
+            f"Protocols Matched: "
+            f"{websocket.subprotocol}"
+        )
     else:
         # In the websockets lib if no subprotocols are supported by the
         # client and the server, it proceeds without a subprotocol,
         # so we have to manually close the connection.
         logging.warning(
-            f"Protocols Mismatched | Expected Subprotocols: {websocket.available_subprotocols},"
-            f" but client supports  {requested_protocols} | Closing connection"
+            f"Protocols Mismatched | Expected Subprotocols: "
+            f"{websocket.available_subprotocols},"
+            f" but client supports  {requested_protocols} | "
+            f"Closing connection"
         )
         return await websocket.close()
 
@@ -63,10 +65,14 @@ async def on_connect(websocket):
 
 async def main():
     server = await websockets.serve(
-        on_connect, "0.0.0.0", 9000, subprotocols=["ocpp1.6"]
+        on_connect, "0.0.0.0",
+        9000,
+        subprotocols=["ocpp1.6"]
     )
 
-    logging.info("Server Started listening to new connections...")
+    logging.info(
+        "Server Started listening to new connections..."
+    )
     await server.wait_closed()
 
 
